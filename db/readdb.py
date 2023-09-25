74e4a57aab4af9c42cb6c39eda801f6ecba02a69
Đức Chính Phạm <102411869+deltaDC@users.noreply.github.com>
2023-09-25 16:46:42 +0700

    add db

---
+++ db/readdb.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+# Kết nối đến cơ sở dữ liệu
+conn = sqlite3.connect('db/app.db')
+cursor = conn.cursor()
+
+# Thực hiện truy vấn SQL
+cursor.execute("SELECT * FROM financial_transaction")
+records = cursor.fetchall()
+
+# In dữ liệu
+for record in records:
+    print(record)
+
+# Đóng kết nối
+conn.close()
