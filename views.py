3e567b88059aa676119d1d8e522c38d4deafa845
DuongDuyLong <113439885+duongduylong@users.noreply.github.com>
2023-10-08 01:38:39 +0700

    Add files via upload

--- views.py
+++ views.py
@@ -7,7 +7,9 @@ from pages.reports.report_income import Report1
 from pages.other import Other
 from pages.reports.report_year_outcome import Report2
 from pages.reports.report_year_income import Report3
-
+from pages.reports.report_toanki import Report4
+from pages.reports.report_danhmuctoanki1 import Report5
+from pages.reports.report_danhmuctoanki2 import Report6
 def views_handler(page):
     return {
         '/':View(
@@ -56,6 +58,24 @@ def views_handler(page):
             route='/report_3',
             controls=[
                 Report3(page)
+            ]
+        ),
+        '/report_4':View(
+            route='/report_4',
+            controls=[
+                Report4(page)
+            ]
+        ),
+        '/report_5':View(
+            route='/report_5',
+            controls=[
+                Report5(page)
+            ]
+        ),
+        '/report_6':View(
+            route='/report_6',
+            controls=[
+                Report6(page)
             ]
         ),
     }
